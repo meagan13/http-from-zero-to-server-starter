b16f4501393d9b06ba95169f9f7a6ba92727f868
meagan <meagan.h.smith@gmail.com>
2021-04-12 15:42:44 -0500

    Complete step one - basic server setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+
+http.createServer(function (req, res) {
+    response.end();
+}).listen(3000, function() {
+    console.log("Listening for requests on port 3000...");
+});
