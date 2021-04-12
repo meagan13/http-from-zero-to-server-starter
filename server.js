887e8ed46f721bdcc65429319a581f17f7257af2
meagan <meagan.h.smith@gmail.com>
2021-04-12 16:27:22 -0500

    Complet phase 2B - Add more codes

--- server.js
+++ server.js
@@ -5,6 +5,38 @@ const server = http.createServer(function (req, res) {
         console.log("Inbound 'OK' request being processed...")
         res.writeHead(200);
         res.end();
+    } else if (req.url === "/Bad-Request") {
+        console.log("Ecountered 400 due to client error");
+        res.writeHead(400);
+        res.end();
+    } else if (req.url === "/Created") {
+        console.log("Succesfully created resource: 201");
+        res.writeHead(201);
+        res.end();
+    } else if (req.url === "/Forbidden") {
+        console.log("No authorization: 403");
+        res.writeHead(403);
+        res.end();
+    } else if (req.url === "/Found") {
+        console.log("Redirected to temporary URL: 302");
+        res.writeHead(302);
+        res.end();
+    } else if (req.url === "/Gateway-Timeout") {
+        console.log("Get not get response in time from upstream server: 504");
+        res.writeHead(504);
+        res.end();
+    } else if (req.url === "Internal-Server-Error") {
+        console.log("Encountered unexpected condition: 500");
+        res.writeHead(500);
+        res.end();
+    } else if (req.url === "/Moved-Permanently") {
+        console.log("Moved to permanent URL: 301");
+        res.writeHead(301);
+        res.end();
+    } else if (req.url === "/Unauthorized") {
+        console.log("No authorization/missing authentication: 401");
+        res.writeHead(401);
+        res.end();
     } else {
         console.log("Sorry encountered 404")
         res.writeHead(404);
